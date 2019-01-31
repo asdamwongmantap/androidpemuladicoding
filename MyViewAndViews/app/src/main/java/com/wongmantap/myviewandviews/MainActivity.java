7893afe364a391535a0e9ebc8ca02b1af2ec62e8
Amiruddin Saddam <asdamgrimson@gmail.com>
2019-01-31 12:12:12 +0700

    Initial Commit

---
+++ MyViewAndViews/app/src/main/java/com/wongmantap/myviewandviews/MainActivity.java
@@ -0,0 +1,18 @@
+package com.wongmantap.myviewandviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ubah title bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("News");
+        }
+    }
+}
